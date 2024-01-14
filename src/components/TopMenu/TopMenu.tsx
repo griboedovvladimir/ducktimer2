cff4636518793890ca7a5b57b45a98d47975bdbd
vgriboedov <v.griboedov@itransition.com>
2024-01-14 18:46:20 +0100

    Resolve condition fetch

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from 'antd';
 import React from 'react';
 
 import { Clear, Load, Save, Timer } from '../../shared/icons';
@@ -5,10 +6,18 @@ import { Clear, Load, Save, Timer } from '../../shared/icons';
 export const TopMenu = ({ addTimer, clearBoard }: any) => {
   return (
     <div id="controlpanel">
-      <Timer title="Add" className="controlpanel icon2" onClick={addTimer} />
-      <Load title="Load board" className="controlpanel icon2" />
-      <Save title="Save board" className="controlpanel icon2" />
-      <Clear title="Clear board" onClick={clearBoard} className="controlpanel icon2" />
+      <Tooltip title="Add">
+        <Timer className="controlpanel icon2" onClick={addTimer} />
+      </Tooltip>
+      <Tooltip title="Load board">
+        <Load className="controlpanel icon2" />
+      </Tooltip>
+      <Tooltip title="Save board">
+        <Save className="controlpanel icon2" />
+      </Tooltip>
+      <Tooltip title="Clear board">
+        <Clear onClick={clearBoard} className="controlpanel icon2" />
+      </Tooltip>
     </div>
   );
 };
