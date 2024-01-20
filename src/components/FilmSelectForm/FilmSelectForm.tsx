962a47b4d126fe2a5bf5844130518c3397e960f4
vgriboedov <v.griboedov@itransition.com>
2024-01-20 10:53:44 +0100

    Fix manifest

--- src/components/FilmSelectForm/FilmSelectForm.tsx
+++ src/components/FilmSelectForm/FilmSelectForm.tsx
@@ -33,8 +33,8 @@ export const FilmSelectForm = () => {
     setFilmFormModel({ ...filmFormModel, [type]: value });
   };
 
-  if(!isLoading && !error && !filmsOptions?.films.length && !filmsOptions?.developers.length) {
-    return <p>Selected film and developer can't use together</p>
+  if (!isLoading && !error && !filmsOptions?.films.length && !filmsOptions?.developers.length) {
+    return <p>{`Selected film and developer can't use together`}</p>;
   }
 
   // eslint-disable-next-line no-nested-ternary
