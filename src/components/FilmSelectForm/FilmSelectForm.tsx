2b11a6a6f337103e18676c0004c26990b19b4a3f
vgriboedov <v.griboedov@itransition.com>
2024-01-21 17:50:16 +0100

    Fix form requests

--- src/components/FilmSelectForm/FilmSelectForm.tsx
+++ src/components/FilmSelectForm/FilmSelectForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { useFetchFilmsPropertiesMutation, useFetchFilmsQuery } from '../../services/filmApiService';
 import { FilmPropertiesForm } from '../FilmPropertiesForm';
@@ -12,68 +12,40 @@ export enum FilmFormFields {
 export const FilmSelectForm = () => {
   const { data: filmsOptions, isLoading, error } = useFetchFilmsQuery('');
   const [trigger, { data: filmProperties }] = useFetchFilmsPropertiesMutation();
+  const film = useRef<any>();
+  const type = useRef<any>();
+  const dev = useRef<any>();
 
   const [isFilmSelected, setIsFilmSelected] = useState(false);
-  const [filmFormModel, setFilmFormModel] = useState({
-    film: '',
-    type: '35mm',
-    dev: '',
-  });
-
-  useEffect(() => {
-    if (filmsOptions) {
-      setFilmFormModel({
-        ...filmFormModel,
-        film: filmsOptions.films[0],
-        dev: filmsOptions.developers[0],
-      });
-    }
-  }, [filmFormModel, filmsOptions]);
-
-  const onChangeFormFields = (type: FilmFormFields, value: string): void => {
-    setFilmFormModel({ ...filmFormModel, [type]: value });
-  };
 
   const onFilmSelect = () => {
     setIsFilmSelected(true);
-    trigger({ film: filmFormModel.film, dev: filmFormModel.dev });
+    trigger({ film: film.current.value, dev: dev.current.value });
   };
 
   // eslint-disable-next-line no-nested-ternary
   return isLoading ? (
     <p>Loading...</p>
-  ) : error ? (
+  ) : !error ? (
     <p>Something went wrong</p>
   ) : (
     <form>
       <p>Select film, film type and developer</p>
-      <select
-        defaultValue={filmsOptions?.films[0]}
-        onChange={({ target }) => onChangeFormFields(FilmFormFields.Film, target.value)}
-        name="film-select"
-      >
-        {filmsOptions?.films.map((film) => <option>{film}</option>)}
+      <select defaultValue={filmsOptions?.films[0]} name="film-select" ref={film}>
+        {filmsOptions?.films.map((filmOption) => <option>{filmOption}</option>)}
       </select>
-      <select
-        onChange={({ target }) => onChangeFormFields(FilmFormFields.Type, target.value)}
-        name="film-type-select"
-        defaultValue="35mm"
-      >
+      <select name="film-type-select" defaultValue="35mm" ref={type}>
         <option>35mm</option>
         <option>120</option>
         <option>sheet</option>
       </select>
-      <select
-        defaultValue={filmsOptions?.developers[0]}
-        onChange={({ target }) => onChangeFormFields(FilmFormFields.Dev, target.value)}
-        name="dev-select"
-      >
+      <select defaultValue={filmsOptions?.developers[0]} name="dev-select" ref={dev}>
         {filmsOptions?.developers.map((developer) => <option>{developer}</option>)}
       </select>
       <button className="trans-color-btn" onClick={() => onFilmSelect()} type="button">
         Next
       </button>
-      {isFilmSelected && <FilmPropertiesForm {...filmFormModel} filmProperties={filmProperties} />}
+      {isFilmSelected && <FilmPropertiesForm film={film} dev={dev} type={type} filmProperties={filmProperties} />}
     </form>
   );
 };
