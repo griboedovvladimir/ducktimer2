4858a810d56204198c2c57db3c561b6d65f38b87
vgriboedov <v.griboedov@itransition.com>
2024-01-27 16:20:27 +0100

    Change login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,5 @@
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+import { Button, Checkbox, Form, Typography, Input, Card } from 'antd';
 import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 
@@ -15,6 +8,14 @@ import { filmApiService } from '../../services/filmApiService';
 import { storageService } from '../../services/storage.service';
 import { Logo } from '../../shared/icons';
 import styles from './Login.module.css';
+
+const onFinishFailed = () => {};
+
+type FieldType = {
+  email?: string;
+  password?: string;
+  remember?: string;
+};
 
 export const Login = () => {
   const navigate = useNavigate();
@@ -27,23 +28,7 @@ export const Login = () => {
     remember: false,
   };
 
-  const onChangeSignIn = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    switch (event.target.id) {
-      case 'email':
-        signIn.email = event.target.value;
-        break;
-      case 'password':
-        signIn.password = event.target.value;
-        break;
-      case 'remember':
-        signIn.remember = event.target.checked;
-        break;
-      default:
-    }
-  };
-
-  const onSubmit = (event: any): void => {
-    event.preventDefault();
+  const onSubmit = (): void => {
     trigger(signIn)
       .unwrap()
       .then((response) => {
@@ -64,45 +49,54 @@ export const Login = () => {
   return (
     <main className={styles.main}>
       <CssBaseline />
-      <Paper className={styles.paper}>
+      <Card className={styles.paper}>
         <Logo className={styles.logo} />
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form onSubmit={onSubmit} className={styles.form} color="secondary">
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input onChange={onChangeSignIn} disableUnderline id="email" name="email" autoComplete="email" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input
-              onChange={onChangeSignIn}
-              disableUnderline
-              name="password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox onChange={onChangeSignIn} value="remember" id="remember" color="secondary" />}
-            label="Remember me"
-          />
-          <Button type="submit" fullWidth variant="contained" color="secondary" className={styles.submit}>
-            Sign in
-          </Button>
-          {showMessage && (
-            <Typography component="p" variant="subtitle2" color="secondary">
-              This user does not exist, maybe your password or email is not correct
-            </Typography>
-          )}
-          <Typography component="p" variant="subtitle2">
-            If you are unable to authorize, you may need to{' '}
-            <NavLink to={ROUTE_CONSTANTS.REGISTRATION_PAGE}>register</NavLink>
-          </Typography>
-        </form>
-      </Paper>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onSubmit}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input bordered={false} />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password bordered={false} />
+          </Form.Item>
+
+          <Form.Item<FieldType> name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        {showMessage && (
+          <Typography.Paragraph color="secondary">
+            This user does not exist, maybe your password or email is not correct
+          </Typography.Paragraph>
+        )}
+        <Typography.Paragraph>
+          If you are unable to authorize, you may need to{' '}
+          <NavLink to={ROUTE_CONSTANTS.REGISTRATION_PAGE}>register</NavLink>
+        </Typography.Paragraph>
+      </Card>
     </main>
   );
 };
