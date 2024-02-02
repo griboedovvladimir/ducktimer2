bad34b96f756362e20bebd770095c1dd71194e58
vgriboedov <v.griboedov@itransition.com>
2024-02-03 00:10:24 +0100

    Fix login

--- src/components/App/AppRoutes.tsx
+++ src/components/App/AppRoutes.tsx
@@ -15,7 +15,7 @@ interface IAppRoutes {
 export const AppRoutes = () => {
   const routes: IAppRoutes[] = [
     {
-      path: ROUTE_CONSTANTS.ROOT,
+      path: ROUTE_CONSTANTS.MAIN_PAGE,
       component: <Main />,
     },
     {
